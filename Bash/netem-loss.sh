3b2f21906cf313ccb80d6913e2c1df2147c44aaf
Charles Torre <torre_charles@hotmail.com>
2017-07-02 16:49:13 -0700

    Added Bash

---
+++ Bash/netem-loss.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+# bash netem-loss.sh -ips=... 5 30s # Random, 5% loss for specified ips for 30s...
+# To make this less random (and to emulate burst), add another percentage value after lossrate in the tc qdisc for loss... -CT
+interface=$(ip -o link show | awk '{print $2,$9}' | grep UP | awk '{str = $0; sub(/: UP/,"",str); print str}')
+# vars
+lossrate="$2%"
+duration="$3"
+burst=""
+if [ -n "$4" ]
+	then
+		burst="$4%"
+fi
+TC=/sbin/tc 
+# Get the comma-delimited ip param value, set to var ipstring... 
+for i in "$@"
+do
+	case $i in
+		-ips=*)
+		ipstring="${i#*=}"
+		shift # past argument=value
+		;;
+		*)
+		        # unknown option
+		;;
+	esac
+done
+# Split ipstring, store into array ips
+declare -a ips
+ips=(${ipstring//,/ })
+# clear existing qdiscs... reinit... suppress error if file doesn't exist...
+$TC qdisc del dev $interface root    2> /dev/null > /dev/null
+# create the qdisc for interface
+$TC qdisc add dev $interface root handle 1: prio
+# loop through ips array and set shaping filters per ip...
+for address in "${ips[@]}"
+do
+	if [[ $address =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; #ipv4
+	then
+			$TC filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $address flowid 1:1
+			$TC filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip src $address flowid 1:1
+			echo "IPv4 Packets to/from IP $address set for random loss with $lossrate drop rate (random means random...)"
+	else
+			$TC filter add dev $interface parent 1:0 protocol ipv6 prio 3 u32 match ip6 dst $address flowid 1:1
+			$TC filter add dev $interface parent 1:0 protocol ipv6 prio 4 u32 match ip6 src $address flowid 1:1
+			echo "IPv6 Packets to/from IP $address set for random loss with $lossrate drop rate (random means random...)"
+	fi
+
+done
+# create the qdisc under existing root qdisc... establishing delay using netem loss...
+$TC qdisc add dev $interface parent 1:1 handle 2: netem loss $lossrate $burst
+
+# keep configuration for the allotted time, then delete the qdiscs for $interface
+sleep $duration
+$TC qdisc del dev $interface root    2> /dev/null > /dev/null
---
+++ Bash/stress-cpu.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+pressurelevel="$1"
+duration="$2"
+echo "Stressing all CPUs at $pressurelevel% capacity for $duration..."
+stress-ng -c 0 -l $pressurelevel -t $duration

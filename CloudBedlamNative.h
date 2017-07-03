5614f30add38d98206ccf28041335b57f92f3ee5
Charles Torre <torre_charles@hotmail.com>
2017-07-02 22:17:58 -0700

    Added bash stuff... need to fix fork() code......

--- CloudBedlamNative.h
+++ CloudBedlamNative.h
@@ -12,6 +12,10 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <string>
+#include <sys/wait.h>
+#include <zconf.h>
+#include <signal.h>
+#include <cstring>
 
 #define	ALL_PROTOCOL 0
 #define	ICMP 1
@@ -56,18 +60,22 @@ struct Endpoint
 typedef enum
 {
     Bandwidth,
+    Corruption,
     Disconnect,
     Latency,
-    Loss
+    Loss,
+    Reorder
 } EmulationType;
 
 static map<string, EmulationType> EmStringValues;
 map<string, EmulationType> MapStringToEmulationType =
         {
-                { "Bandwidth", Bandwidth },
-                { "Disconnect", Disconnect },
-                { "Latency", Latency },
-                { "Loss", Loss }
+            { "Bandwidth", Bandwidth },
+            { "Corruption", Corruption },
+            { "Disconnect", Disconnect },
+            { "Latency", Latency },
+            { "Loss", Loss },
+            { "Reorder", Reorder }
         };
 
 struct NetworkEmulationProfile
@@ -75,23 +83,24 @@ struct NetworkEmulationProfile
     vector<Endpoint> Endpoints;
     int Duration = 0;
     int RunOrder = 0;
+    EmulationType Type;
 };
 
-inline wstring Hostname2Ips(const wstring &hostname, const wstring &port)
+inline string hostname2ips(const string &hostname)
 {
+    struct addrinfo* addrinfo = nullptr;
+    struct addrinfo hints;
 
-    return L"";
-}
-
-inline string hostname2ips(string &hostname)
-{
-    struct addrinfo * _addrinfo;
-    struct addrinfo * _res;
-    char _address[INET6_ADDRSTRLEN];
     int err = 0;
     string ips;
 
-    err = getaddrinfo(hostname.c_str(), nullptr, nullptr, &_addrinfo);
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;          /* Any protocol */
+
+    err = getaddrinfo(hostname.c_str(), nullptr, &hints, &addrinfo);
 
     if(err != 0)
     {
@@ -99,48 +108,39 @@ inline string hostname2ips(string &hostname)
         return "";
     }
 
-    for(_res = _addrinfo; _res != nullptr; _res = _res->ai_next)
+    for(auto ptr = addrinfo; ptr != nullptr; ptr = ptr->ai_next)
     {
-        if(_res->ai_family == AF_INET)
+        if(ptr->ai_family == AF_INET)
         {
+            char address[INET6_ADDRSTRLEN];
+            /* Obtain address(es) matching host/port */
 
-            if(inet_ntop(AF_INET, &((struct sockaddr_in *)_res->ai_addr)->sin_addr, _address, sizeof(_address)) == nullptr)
+            if(inet_ntop(AF_INET, &((struct sockaddr_in *)ptr->ai_addr)->sin_addr, address, sizeof(address)) == nullptr)
             {
                 return "";
             }
-            string s(_address);
+            string s(address);
             wstring loginfo = L"\nIPv4 - ";
             wstring ip(s.begin(), s.end());
             loginfo += ip;
             ips += s + ",";
         }
-        else if(_res->ai_family == AF_INET6)
+        else if(ptr->ai_family == AF_INET6)
         {
-            if(inet_ntop(AF_INET6, &((struct sockaddr_in *)_res->ai_addr)->sin_addr, _address, sizeof(_address)) == nullptr)
+            char address[INET6_ADDRSTRLEN];
+            if(inet_ntop(AF_INET6, &((struct sockaddr_in *)ptr->ai_addr)->sin_addr, address, sizeof(address)) == nullptr)
             {
                 return "";
             }
-            string s(_address);
+            string s(address);
             wstring loginfo = L"\nIPv6 - ";
             wstring ip(s.begin(), s.end());
             loginfo += ip;
             ips += s + ",";
         }
     }
-}
-
-
-inline vector<wstring> split(const wstring &s, wchar_t delim)
-{
-    vector<wstring> elems;
-    wstringstream ss;
-    ss.str(s);
-    wstring item;
-    while (getline(ss, item, delim))
-    {
-        elems.push_back(item);
-    }
-    return elems;
+    freeaddrinfo(addrinfo);           /* No longer needed */
+    return ips;
 }
 
 // Fault objects...
@@ -150,24 +150,21 @@ struct CpuPressure
 	int PressureLevel = 0;
 	int Duration = 0;
 	//int RunOrder = 0;
-	wstring ExePath = L"BedlamBinaries\\Stressfulx64.exe";
-	wstring RunCommand;
+	string CmdArgs = "Bash/stress-cpu.sh ";
 };
 struct MemoryPressure
 {
 	int PressureLevel = 0;
 	int Duration = 0;
 	//int RunOrder = 0;
-	wstring ExePath = L"BedlamBinaries\\testlimitx64.exe";
-	wstring RunCommand;
+	string CmdArgs = "Bash/stress-mem.sh ";
 };
 
 //Globals...
-wstring g_logfilepath;
-wstring g_execPath;
+string g_logfilepath;
 int g_repeat = 0;
 Json g_json = nullptr;
-
+int g_delay;
 CpuPressure Cpu;
 MemoryPressure Mem;
 NetworkEmulationProfile Netem;
@@ -209,28 +206,37 @@ map<string, NetworkType> MapStringToNetworkType =
 	{ "IPv6", Ipv6 },
 };
 
-inline unsigned long long GetAvailableSystemMemory()
+inline bool RunOperation(string command, int duration = 0)
 {
-	//MEMORYSTATUSEX status;
-	//status.dwLength = sizeof(status);
-	//GlobalMemoryStatusEx(&status);
-	//return status.ullAvailPhys;
-}
-
-inline int GetMemPressureAllocation(int pressureLevelPt)
-{
-	auto allocation = static_cast<int>(round(pressureLevelPt / 100.0 * GetAvailableSystemMemory() / (1024.0 * 1024.0)));
-	return allocation;
-}
-
-inline bool RunOperation(const wstring &command, int duration)
-{
-	if(duration/1000 < 1)
+	if(duration > 0 && duration/1000 < 1)
 	{
 		duration *= 1000; //to ms...
 	}
-	auto cmd = command.c_str();
-	
+
+    //convert string to char*...
+    char *cstr = &command[0u];
+
+    int status, ret, pid;
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        //child process...
+        execv("/usr/bin/bash", &cstr);
+    }
+
+    if ((ret = waitpid(pid, &status, 0)) == -1)
+    {
+        printf ("parent:error\n");
+        return false;
+    }
+
+    if (ret == pid)
+    {
+        printf ("Parent: Child process waited for.\n");
+    }
+
 	return true;
 }
 //CPU
@@ -256,11 +262,10 @@ inline void RunCpuPressure()
 			level = g_json["ChaosConfiguration"]["CpuPressure"]["PressureLevel"].int_value();
 			Cpu.PressureLevel = level;
 		}
-		
-		Cpu.RunCommand = g_execPath + L"\\" + Cpu.ExePath + L" " + wstring(to_wstring(Cpu.PressureLevel));
+		Cpu.CmdArgs += to_string(Cpu.PressureLevel) + " " + to_string(Cpu.Duration);
 		wstring loginfo = L"Starting CPU pressure: " + wstring(to_wstring(level));
 		//LOGINFO(g_logger, loginfo.c_str());
-		if (RunOperation(Cpu.RunCommand, Cpu.Duration))
+		if (RunOperation(Cpu.CmdArgs))
 		{
 			//LOGINFO(g_logger, L"Stopping CPU pressure.")
 		}
@@ -297,12 +302,11 @@ inline void RunMemoryPressure()
 			level = g_json["ChaosConfiguration"]["MemoryPressure"]["PressureLevel"].int_value();
 			Mem.PressureLevel = level;
 		}
-		
-		Mem.RunCommand = g_execPath + L"\\" + Mem.ExePath + L" -d " + wstring(to_wstring(GetMemPressureAllocation(Mem.PressureLevel))) + L" -c 1";
+		Mem.CmdArgs += to_string(Mem.PressureLevel) + " " + to_string(Mem.Duration);
 		wstring loginfo = L"Starting memory pressure: " + wstring(to_wstring(level));
 		//LOGINFO(g_logger, wstring(loginfo.begin(), loginfo.end()).c_str());
 
-		if (RunOperation(Mem.RunCommand, Mem.Duration))
+		if (RunOperation(Mem.CmdArgs))
 		{
 			//LOGINFO(g_logger, L"Stopping Memory pressure.")
 		}
@@ -326,13 +330,12 @@ inline void RunNetworkEmulation()
 
 	bool isNetemObj = g_json["ChaosConfiguration"]["NetworkEmulation"].is_object();
 	if (isNetemObj)
-	{
-		NetworkEmulationProfile profile;
+    {
 		bool isDuration = g_json["ChaosConfiguration"]["NetworkEmulation"]["Duration"].is_number();
 		if(isDuration)
 		{
 			int duration = g_json["ChaosConfiguration"]["NetworkEmulation"]["Duration"].int_value();
-			profile.Duration = duration;
+            Netem.Duration = duration;
 		}
 		else
 		{
@@ -359,7 +362,9 @@ inline void RunNetworkEmulation()
 
             }
         }
-		
+        //remove trailing character (in our case, a comma will always be the last char...)
+        ips = ips.substr(0, ips.size() - 1);
+
 		string s_emType = g_json["ChaosConfiguration"]["NetworkEmulation"]["EmulationType"].string_value();
 		wstring netemLogType;
 		EmulationType emType = MapStringToEmulationType[s_emType];
@@ -369,52 +374,73 @@ inline void RunNetworkEmulation()
 		{
 			case Bandwidth:
 			{
-				//profile.Type = Bandwidth;
+				Netem.Type = Bandwidth;
 				netemLogType = L"Bandwidth emulation";
 				int upbw = g_json["ChaosConfiguration"]["NetworkEmulation"]["BandwidthUpstreamSpeed"].int_value();
 				int dbw = g_json["ChaosConfiguration"]["NetworkEmulation"]["BandwidthDownstreamSpeed"].int_value();
-				auto DownlinkBandwidthBps = dbw;
-				auto UplinkBandwidthBps = upbw;
-                bashCmd = "Bash/netem-bandwidth.sh -ips=" + ips.substr(0, ips.size()-1) + " " +
-                          to_string(dbw) + " " + to_string(profile.Duration) + "s";
+
+                bashCmd = "Bash/netem-bandwidth.sh -ips=" + ips + " " + to_string(dbw) + " " + to_string(Netem.Duration) + "s";
 				break;
 			}
-			case Disconnect: //TODO for linux...
+            case Corruption:
+            {
+                Netem.Type = Corruption;
+                netemLogType = L"Corruption emulation";
+                auto pt = g_json["ChaosConfiguration"]["NetworkEmulation"]["PacketPercentage"].int_value() * 100; //e.g., 0.05 * 100 = 5...
+                bashCmd = "Bash/netem-corrupt.sh -ips=" + ips + " " +
+                       to_string(pt) + " " + to_string(Netem.Duration) + "s";
+                break;
+            }
+			case Disconnect: //TODO...
 			{
-				//profile.Type = Disconnect;
-				netemLogType = L"Disconnection emulation";
+				/*
+                netemLogType = L"Disconnection emulation";
 
 				auto PeriodicDisconnectionRate = g_json["ChaosConfiguratioin"]["NetworkEmulation"]["PeriodicDisconnectionRate"].int_value();
 				auto ConnectionTime = (unsigned int) g_json["ChaosConfiguration"]["NetworkEmulation"]["ConnectionTime"].int_value();
 				auto DisconnectionTime = (unsigned int) g_json["ChaosConfiguration"]["NetworkEmulation"]["DisconnectionTime"].int_value();
-				break;
+				*/
+                break;
 			}
-			case Loss: //TODO
+			case Loss:
 			{
-				netemLogType = L"Loss (Random) emulation";
-				//auto inloss = g_json["ChaosConfiguration"]["NetworkEmulation"]["RandomLossRate"].int_value();
+				Netem.Type = Loss;
+                netemLogType = L"Loss (Random) emulation";
+				auto inloss = g_json["ChaosConfiguration"]["NetworkEmulation"]["RandomLossRate"].int_value();
+                auto lossRate = inloss * 100; //e.g., 0.05 * 100 = 5...
+
+                bashCmd = "Bash/netem-loss.sh -ips=" + ips + " " + to_string(lossRate) + " " + to_string(Netem.Duration);
 				break;
 			}
 			case Latency:
 			{
-				//profile.Type = Latency;
-				netemLogType = L"Latency emulation";
+				Netem.Type = Latency;
+                netemLogType = L"Latency emulation";
 				int delay = g_json["ChaosConfiguration"]["NetworkEmulation"]["LatencyDelay"].int_value();
-				//profile.Latency.Type = FIXED_LATENCY;
-				auto IncomingLatency = delay;
-				auto OutgoingLatency = delay;
+
+                bashCmd = "Bash/netem-latency.sh -ips=" + ips + " " + to_string(delay) + "ms " + " " + to_string(Netem.Duration) + "s";
 				break;
 			}
+            case Reorder:
+            {
+                Netem.Type = Reorder;
+                netemLogType = L"Reorder emulation";
+                auto correlationpt = g_json["ChaosConfiguration"]["NetworkEmulation"]["CorrelationPercentage"].int_value() * 100;
+                auto packetpt = g_json["ChaosConfiguration"]["NetworkEmulation"]["PacketPercentage"].int_value() * 100;
+
+                bashCmd = "Bash/netem-reorder.sh -ips=" + ips + " " + to_string(packetpt) + " " + to_string(correlationpt) + " " + to_string(Netem.Duration);
+                break;
+            }
 			default:
-				{
-					//LOGINFO(g_logger, L"Emulation type unrecognized.");
-					return;
-				}
+            {
+                //LOGINFO(g_logger, L"Emulation type unrecognized.");
+                return;
+            }
 		}
 
 		auto loginfo = L"Starting " + netemLogType + L" for " + hostnames + L" }";
 		//LOGINFO(g_logger, loginfo.c_str());
-		//RunNetworkEmulator(profile);
+        RunOperation(bashCmd);
 		//LOGINFO(g_logger, L"Stopping network emulation")
 	}
 	else
@@ -438,6 +464,7 @@ inline void InitGlobalPaths()
 	//	g_logger.AddOutputStream(new wofstream(g_logfilepath.c_str()), true, LogLevel::Error);
 	//}
 }
+
 inline bool ParseConfigurationObjectAndInitialize()
 {
 	if (g_json == nullptr)//so, first run (not a repeat run...)...
@@ -459,81 +486,82 @@ inline bool ParseConfigurationObjectAndInitialize()
 			//LOGERROR(g_logger, L"Can't open configuration file. Aborting...");
 			exit(-1);
 		}
-	}
 
-	bool isChaosConfigObj = g_json["ChaosConfiguration"].is_object();
-	if (isChaosConfigObj)
-	{
-		auto isOrch = g_json["ChaosConfiguration"]["Orchestration"].is_string();
-		if (isOrch)
-		{
-			string orc = g_json["ChaosConfiguration"]["Orchestration"].string_value();
-			g_orchestration = MapStringToOrchestration[orc];
-			string orch = "Orchestration: " + orc;
-			
-			//LOGINFO(g_logger, wstring(orch.begin(), orch.end()).c_str());
-		}
-		auto isRepeat = g_json["ChaosConfiguration"]["Repeat"].is_number();
-		if (isRepeat)
-		{
-			g_repeat = g_json["ChaosConfiguration"]["Repeat"].int_value();
-		}
-		auto isDelay = g_json["ChaosConfiguration"]["RunDelay"].is_number();
-		if (isDelay)
-		{
-			//sleep(g_json["ChaosConfiguration"]["RunDelay"].int_value() * 1000);
-		}
-		//What's in the config?...
-		bool isCpuObject = g_json["ChaosConfiguration"]["CpuPressure"].is_object();
-		if (isCpuObject)
-		{
-			g_bCPUConfig = true;
-		}
-		//Mem
-		bool isMemObject = g_json["ChaosConfiguration"]["MemoryPressure"].is_object();
-		if (isMemObject)
-		{
-			g_bMemConfig = true;
-		}
-		//Netem
-		bool isNetemObject = g_json["ChaosConfiguration"]["NetworkEmulation"].is_object();
-		if (isNetemObject)
-		{
-			g_bNetemConfig = true;
-		}
-		//If Sequential orc, then set runorders...
-		if (g_orchestration == Orchestration::Sequential)
-		{
-			if (g_bCPUConfig)
-			{
-				bool isRunOrder = g_json["ChaosConfiguration"]["CpuPressure"]["RunOrder"].is_number();
-				if (isRunOrder)
-				{
-					int r = g_json["ChaosConfiguration"]["CpuPressure"]["RunOrder"].int_value();
-					seqrunVec.emplace_back(RunCpuPressure, r);
-				}
-			}
-			//Mem
-			if (g_bMemConfig)
-			{
-				bool isRunOrder = g_json["ChaosConfiguration"]["MemoryPressure"]["RunOrder"].is_number();
-				if (isRunOrder)
-				{
-					int r = g_json["ChaosConfiguration"]["MemoryPressure"]["RunOrder"].int_value();
-					seqrunVec.emplace_back(RunMemoryPressure, r);
-				}
-			}
-			//Netem
-			if (g_bNetemConfig)
-			{
-				bool isRunOrder = g_json["ChaosConfiguration"]["NetworkEmulation"]["RunOrder"].is_number();
-				if (isRunOrder)
-				{
-					int r = g_json["ChaosConfiguration"]["NetworkEmulation"]["RunOrder"].int_value();
-					seqrunVec.emplace_back(RunNetworkEmulation, r);
-				}
-			}
-		}
+        bool isChaosConfigObj = g_json["ChaosConfiguration"].is_object();
+        if (isChaosConfigObj)
+        {
+            auto isOrch = g_json["ChaosConfiguration"]["Orchestration"].is_string();
+            if (isOrch)
+            {
+                string orc = g_json["ChaosConfiguration"]["Orchestration"].string_value();
+                g_orchestration = MapStringToOrchestration[orc];
+                string orch = "Orchestration: " + orc;
+
+                //LOGINFO(g_logger, wstring(orch.begin(), orch.end()).c_str());
+            }
+            auto isRepeat = g_json["ChaosConfiguration"]["Repeat"].is_number();
+            if (isRepeat)
+            {
+                g_repeat = g_json["ChaosConfiguration"]["Repeat"].int_value();
+            }
+            auto isDelay = g_json["ChaosConfiguration"]["RunDelay"].is_number();
+            if (isDelay)
+            {
+                g_delay = g_json["ChaosConfiguration"]["RunDelay"].int_value() * 1000;
+                sleep(g_delay);
+            }
+            //What's in the config?...
+            bool isCpuObject = g_json["ChaosConfiguration"]["CpuPressure"].is_object();
+            if (isCpuObject)
+            {
+                g_bCPUConfig = true;
+            }
+            //Mem
+            bool isMemObject = g_json["ChaosConfiguration"]["MemoryPressure"].is_object();
+            if (isMemObject)
+            {
+                g_bMemConfig = true;
+            }
+            //Netem
+            bool isNetemObject = g_json["ChaosConfiguration"]["NetworkEmulation"].is_object();
+            if (isNetemObject)
+            {
+                g_bNetemConfig = true;
+            }
+            //If Sequential orc, then set runorders...
+            if (g_orchestration == Orchestration::Sequential)
+            {
+                if (g_bCPUConfig)
+                {
+                    bool isRunOrder = g_json["ChaosConfiguration"]["CpuPressure"]["RunOrder"].is_number();
+                    if (isRunOrder)
+                    {
+                        int r = g_json["ChaosConfiguration"]["CpuPressure"]["RunOrder"].int_value();
+                        seqrunVec.emplace_back(RunCpuPressure, r);
+                    }
+                }
+                //Mem
+                if (g_bMemConfig)
+                {
+                    bool isRunOrder = g_json["ChaosConfiguration"]["MemoryPressure"]["RunOrder"].is_number();
+                    if (isRunOrder)
+                    {
+                        int r = g_json["ChaosConfiguration"]["MemoryPressure"]["RunOrder"].int_value();
+                        seqrunVec.emplace_back(RunMemoryPressure, r);
+                    }
+                }
+                //Netem
+                if (g_bNetemConfig)
+                {
+                    bool isRunOrder = g_json["ChaosConfiguration"]["NetworkEmulation"]["RunOrder"].is_number();
+                    if (isRunOrder)
+                    {
+                        int r = g_json["ChaosConfiguration"]["NetworkEmulation"]["RunOrder"].int_value();
+                        seqrunVec.emplace_back(RunNetworkEmulation, r);
+                    }
+                }
+            }
+        }
 		return true;
 	}
 	//LOGERROR(g_logger, L"Invalid Json (Not a ChaosConfiguration...). Make sure you supplied the correct format...");

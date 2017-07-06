ec370e6fee4339078500f6dc6c72e24f7a5d19da
Charles Torre <qedlab@outlook.com>
2017-07-05 21:48:31 -0700

    Clean

--- Bash/stress-mem.sh
+++ Bash/stress-mem.sh
@@ -3,9 +3,9 @@
 # nice and simple... :) -CT
 # bash stress-mem.sh 90 15 # eat 90% of available memory, stop after 15 seconds...
 
-vmbytes=$(awk "/MemFree/ { printf \"%0.3f\", (\$2*0.$1)/1024/1024; }" /proc/meminfo)G
+vmbytes=$(awk "/MemFree/ { printf \"%d\", (\$2*0.$1)/1024/1024; }" /proc/meminfo)G
 
 # This requires stress, try and install, swallow any errors (like if it's already installed, for example...)
 apt install stress  2> /dev/null > /dev/null
-echo "Eating $1% of available memory for $duration"s...
-stress --vm 1 --vm-bytes $vmbytes --vm-keep --timeout $2
+echo "Eating $1% of available memory for $2"s...
+stress --vm 2 --vm-bytes $vmbytes --vm-hang $2 --vm-keep --timeout $2
